b534a57512f8a48fac563db16df5651aed9ff624
jdeepee <joshuadparkin@gmail.com>
2021-07-06 16:06:30 +0100

    sign agent expression before posting into holochain

--- adapter.ts
+++ adapter.ts
@@ -1,27 +1,29 @@
-import type { Address, Expression, PublicSharing, LanguageContext, HolochainLanguageDelegate, AgentAdapter, AgentExpression } from "@perspect3vism/ad4m";
+import type { Address, Expression, PublicSharing, LanguageContext, HolochainLanguageDelegate, AgentAdapter, AgentExpression, AgentService } from "@perspect3vism/ad4m";
 import { DNA_NICK } from "./dna";
 
 export default class AgentAdapterImpl implements AgentAdapter {
   #DNA: HolochainLanguageDelegate;
+  #agent: AgentService;
 
   constructor(context: LanguageContext) {
     this.#DNA = context.Holochain as HolochainLanguageDelegate;
+    this.#agent = context.agent; 
   }
 
   async setProfile(agentExpression: AgentExpression) {
-    const orderedShortFormData = Object.keys(agentExpression)
+    const agentExpressionOrdered = Object.keys(agentExpression)
       .sort()
       .reduce((obj, key) => {
         obj[key] = agentExpression[key];
         return obj;
       }, {});
-    //const expression = this.#agent.createSignedExpression(orderedShortFormData);
-    console.log("Posting agent expression", orderedShortFormData);
+    const expression = this.#agent.createSignedExpression(agentExpressionOrdered);
+    console.log("Posting agent expression", expression);
     await this.#DNA.call(
       DNA_NICK,
       "agent_store",
       "create_agent_expression",
-      orderedShortFormData
+      expression
     );
   };
 

0f57afef27a7ea775cb68bb541442845bc721b39
jdeepee <joshuadparkin@gmail.com>
2021-10-15 14:56:27 +0100

    bump hdk 110

--- hc-dna/default.nix
+++ hc-dna/default.nix
@@ -1,16 +1,16 @@
 let
   holonixPath = builtins.fetchTarball {
-    url = "https://github.com/holochain/holonix/archive/55a5eef58979fb6bc476d8c3e0c028cdeb1b5421.tar.gz";
-    sha256 = "sha256:0q6d0rql1pyy93xw1c8s28jjjcgk1zgwxwixsp9z5r4w2ihaz3zg";
+    url = "https://github.com/holochain/holonix/archive/2a1c9124f52e18e7829bea62e8cc476572a0bd32.tar.gz";
+    sha256 = "sha256:07q0ykvg3wkg35ax7vap7zfsvpfmi5y48c0dvnkww78690a9gwgj";
   };
   holonix = import (holonixPath) {
     includeHolochainBinaries = true;
     holochainVersionId = "custom";
 
     holochainVersion = {
-      rev = "cad04aec3fb5f137b2d224ab29dcc204af7b9821";
-      sha256 = "1p9rqd2d2wlyzc214ia93b1f18fgqspmza863q4hrz9ba6xigzjs";
-      cargoSha256 = "sha256:0p4m8ckbd7v411wgh14p0iz4dwi84i3cha5m1zgnqlln0wkqsb0f";
+      rev = "06c82ade3128ccc73b7b24a137d8fad3756927ef";
+      sha256 = "sha256:1fykfqslr7lhbp11wbl7cz5pmygw9wmhlkvvnfn9ig9ddr7nq6sw";
+      cargoSha256 = "sha256:11s50qq7719grgijnw2z2wi27xa918ycjnsmcd5a8c2kvf4al3yw";
       bins = {
         holochain = "holochain";
         hc = "hc";

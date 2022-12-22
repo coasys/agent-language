c77cbb186930441eaaecbc32f0f539627a43c967
Joshua Parkin <joshuadparkin@gmail.com>
2022-12-21 16:22:07 -0800

    add zome function calls to registerDnas call

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import type { Address, Agent, Language, LanguageContext, HolochainLanguageDelegate, Interaction } from "@perspect3vism/ad4m";
+import type { Address, Language, LanguageContext, HolochainLanguageDelegate, Interaction } from "@perspect3vism/ad4m";
 import ExpressionAdapter from "./adapter";
 import Icon from "./build/Icon.js";
 import ConstructorIcon from "./build/ConstructorIcon.js";
@@ -22,7 +22,15 @@ export const name = "agent-expression-store";
 
 export default async function create(context: LanguageContext): Promise<Language> {
   const Holochain = context.Holochain as HolochainLanguageDelegate;
-  await Holochain.registerDNAs([{ file: DNA, nick: DNA_NICK }]);
+  await Holochain.registerDNAs(
+    //@ts-ignore
+    [{ file: DNA, nick: DNA_NICK, zomeCalls:
+      [
+        ["agent_store", "create_agent_expression"],
+        ["agent_store", "get_agent_expression"]
+      ] 
+    }], 
+  );
 
   const expressionAdapter = new ExpressionAdapter(context);
   const expressionUI = new UI();
